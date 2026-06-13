ff2680d956a587c493c04e8574cb60326f36b186
anthuanProsperia <anthuan@prosperia.ai>
2026-06-13 00:26:28 -0500

    feat: unlock predictions button for user miri

--- src/modules/dashboard/pages/DashboardPage.tsx
+++ src/modules/dashboard/pages/DashboardPage.tsx
@@ -203,7 +203,7 @@ export const DashboardPage = () => {
               </Typography>
               <Grid container spacing={2} sx={{ mt: 1 }}>
                 <Grid size={{ xs: 12, sm: 6 }}>
-                  {stats && (role === 'ADMIN' || stats.hasCompletedQuiniela || user?.user_metadata?.username === 'SirRuben30' || user?.user_metadata?.username === 'Fabian') ? (
+                  {stats && (role === 'ADMIN' || stats.hasCompletedQuiniela || user?.user_metadata?.username === 'SirRuben30' || user?.user_metadata?.username === 'Fabian' || user?.user_metadata?.username === 'miri') ? (
                     <Button fullWidth component={RouterLink} to="/dashboard/predictions" variant="contained" color="primary" sx={{ fontWeight: 800 }}>
                       Mis Predicciones
                     </Button>
